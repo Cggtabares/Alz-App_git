b506a36a3a3b6a7cd0e21ced228e2dd79be021f8
cggtabares <carlosggtabares@gmail.com>
2023-11-16 01:14:00 -0400

    Modified LogIn screeen with email, password and button, this has to be replicated to the others field on other screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.material:material-icons-extended:1.3.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/example/alz_app/Ingresar.kt
+++ app/src/main/java/com/example/alz_app/Ingresar.kt
@@ -2,26 +2,46 @@ package com.example.alz_app
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,49 +50,124 @@ fun IngresarScreen() {
     Scaffold(
         topBar = { TopAppBar(title = { Text("Ingresar") }) },
     ) { innerPadding ->
-        LazyColumn(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Box(
             modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxWidth()
                 .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxWidth()
         ) {
-            item {
-                Image(
-                    painter = painterResource(id = R.drawable.alzlogo),
-                    contentDescription = "Logo Alz-App", Modifier.size(200.dp),
-                )
-            }
-            item {
-                OutlinedTextField(
-                    value = "",
-                    onValueChange = { },
-                    label = { Text("Correo electrónico") }
-                )
-            }
-            item {
-                Spacer(modifier = Modifier.size(16.dp))
-            }
-            item {
-                OutlinedTextField(
-                    value = "",
-                    onValueChange = { },
-                    label = { Text("Contraseña") },
-                )
-            }
-            item {
-                Button(
-                    shape = RoundedCornerShape(10.dp),
-                    onClick = { /* Submit form */ },
-                    content = { Text("Ingresar") },
-                    modifier = Modifier
-                        .width(260.dp)
-                        .padding(top = 32.dp)
-                )
-            }
+
+            Body(Modifier.align(Alignment.Center))
         }
+
     }
+}
+
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var isLoginEnable by rememberSaveable { mutableStateOf(false) }
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
+        Email(email, { email = it })
+        Spacer(modifier = Modifier.size(16.dp))
+        Password(password, { password = it })
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+        LoginButton(isLoginEnable)
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean) {
+    Button(
+        shape = RoundedCornerShape(10.dp),
+        onClick = { /* Submit form */ },
+        enabled = loginEnable,
+        modifier = Modifier
+            .padding(top = 32.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF4EA8E9)
+        )
+    ) {
+        Text("Ingresar")
+    }
+}
+
+@Composable
+fun ImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.alzlogo),
+        contentDescription = "Logo Alz-App", Modifier.size(200.dp),
+    )
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot Password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF000000),
+        modifier = modifier
+
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    OutlinedTextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text("Correo electrónico") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        /*colors = TextFieldDefaults.textFieldColors(
+            //textColor = Color(0xFFB2B2B2),
+            //containerColor = Color(0xFFAFAFAF),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        )*/
+
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Password(password: String, onTextChanged: (String) -> Unit) {
+    var passwordVisibility by remember { mutableStateOf(false) }
+    OutlinedTextField(
+        value = password,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text("Contraseña") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imagen = if (passwordVisibility) {
+                Icons.Filled.VisibilityOff
+            } else {
+                Icons.Filled.Visibility
+            }
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(imageVector = imagen, contentDescription = "Password Visibility")
+
+            }
+        },
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+    )
 }
 
 
